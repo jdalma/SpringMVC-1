588c17c608c5397e8c5cd1cb950f492a95772cdb
정현준 <jeongcode@naver.com>
2022-04-19 23:05:42 +0900

    HTTP 요청 데이터 POST HTML Form 방식

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -31,6 +31,12 @@ public class RequestParamServlet extends HttpServlet {
         }
         // 키 값이 중복일 때 getParameter를 사용하면 getParameterValues의 첫 번째 값을 반환한다.
         System.out.println("\n[이름이 같은 복수 파라미터 조회] - end\n");
+
+        // POST 방식도 위와 같이 getParam 관련 메서드들로도 꺼낼 수 있다.
+        // content-type : HTTP 메시지 바디의 데이터 형식을 지정한다
+        // GET URL 쿼리 파라미터 형식으로 클라이언트에서 서버로 데이터를 전달할 때는 HTTP 메시지 바디를 사용하지 않기 때문에 content-type이 없다
+        // POST HTML FORM 형식으로 데이터를 전달하면 HTTP 메시지 바디에 해당 데이터를 포함해서 보내기 때문에 바디에 포함된 데이터가 어떤 형식인지 content-type을 꼭 지정해야 한다.
+        // 폼으로 데이터를 전송하는 형식을 application/x-www-form-urlencoded라 한다
     }
 
 }
